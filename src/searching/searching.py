9fc41db6fff1ba724597f591d3fc8da05a8c48a0
cjogles <jackson.ogles28@gmail.com>
2020-07-21 13:55:28 -0600

    mvp reached for merge sort and recursive binary search. But I don't know how they work very well... Will continue to study them

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search(arr, target, low, high):
+    # Check base case 
+    if high >= low: 
+  
+        mid = (high + low) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == target: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > target: 
+            return binary_search(arr, target, low, mid - 1) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, target, mid + 1, high) 
+    else: 
+        # Element is not present in the array 
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +30,5 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,15 +2,42 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
+    
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    if len(arr) >1: 
+        mid = len(arr)//2 # Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        merge_sort(L) # Sorting the first half 
+        merge_sort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+= 1
+            else: 
+                arr[k] = R[j] 
+                j+= 1
+            k+= 1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+= 1
+            k+= 1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+= 1
+            k+= 1
 
 
     return arr
@@ -21,8 +48,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
